68a4d3f2698a0355d42e8f48c79eb4d1111df035
muhammadejaz32 <sahil33ks@gmail.com>
2026-01-20 09:45:24 +0500

    modified

--- new.py
+++ new.py
@@ -1,1 +1,24 @@
-print("github")
+list1  =[ 1,3,4]
+string  = "ahmad"
+
+print(len(list1))
+print(len(string))
+
+
+class student:
+    def marks(self, sub1, sub2):
+        self.sub1  = sub1
+        self.sub2  =sub2
+        print(self.sub1+self.sub2)
+        print("parent class")
+
+class stude(student):
+    def marks(self, sub1, sub2):
+        super().marks(10,12)
+        self.sub1  = sub1
+        self.sub2  =sub2
+        print(self.sub1+self.sub2)
+        print("child class")
+
+st2  =stude()
+st2.marks(10,34)
